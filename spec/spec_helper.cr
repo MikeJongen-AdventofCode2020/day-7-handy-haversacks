e1c872df48084439c545b81560de32df991d5387
Mike Jongen <mikejongen@hotmail.com>
2020-12-15 21:36:15 +0100

    Initial commit.

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/day-7-handy-haversacks"
---
+++ src/day-7-handy-haversacks.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Day::7::Handy::Haversacks`
+module Day::7::Handy::Haversacks
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
